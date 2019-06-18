531836aaee8ea74077a9ba4c7bd4b75b8892f1b8
Tomasz Hemperek <hemperek@physik.uni-bonn.de>
2019-06-18 09:58:30 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+from setuptools import find_packages
+
+import cocotb_test
+version = cocotb_test.__version__
+
+from os import path
+
+def read_file(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+setup(
+    name='cocotb-test',
+    version=version,
+    description='',
+    url='',
+    license='BSD',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    author='Tomasz Hemperek',
+    author_email='hemperek@uni-bonn.de',
+    packages=find_packages(),
+    install_requires= ['cocotb'],
+    platforms='any',
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)",
+    ],
+)
