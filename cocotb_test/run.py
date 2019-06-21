f11e49f4fc7c17e0fb365da8ca19e2171fe01a3f
Tomasz Hemperek <hemperek@physik.uni-bonn.de>
2019-06-21 14:35:35 +0200

    Try add LIBDIR

--- cocotb_test/run.py
+++ cocotb_test/run.py
@@ -64,7 +64,10 @@ def build_libs():
 
     python_lib = sysconfig.get_config_var("LDLIBRARY")
     python_lib_link = os.path.splitext(python_lib)[0][3:]
-
+    python_lib_dir = sysconfig.get_config_var("LIBDIR") 
+    
+    print('X'*20, sysconfig.get_config_vars()) 
+    
     dist = Distribution()
     dist.parse_config_files()
 
@@ -81,7 +84,7 @@ def build_libs():
         # define_macros=[("MODELSIM",),("VPI_CHECKING",)],
         include_dirs=[share_dir + "/include"],
         libraries=[python_lib_link, "pthread", "dl", "util", "rt", "m", "cocotbutils"],
-        library_dirs=[lib_path],
+        library_dirs=[python_lib_dir, lib_path],
         sources=[share_dir + "/lib/gpi_log/gpi_logging.c"],
     )
 
