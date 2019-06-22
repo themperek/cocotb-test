2243b568f54bc81b32b81dd6a045b0cfd43a272b
Colin Marquardt <cmarqu42@gmail.com>
2019-06-23 00:51:25 +0200

    Fix small typo.

--- tests/test_dff.py
+++ tests/test_dff.py
@@ -1,14 +1,12 @@
-
 from cocotb_test.run import Run
 
 def test_dff():
     Run(
         verilog_sources=['dff.v'], # sources
         toplevel='dff',            # top level HDL
-        python_search=['.'],       # serch path for cocotb test module
+        python_search=['.'],       # search path for cocotb test module
         module='dff_cocotb'        # name of cocotb test module
     )
 
 if __name__ == "__main__":
     test_dff()
-
