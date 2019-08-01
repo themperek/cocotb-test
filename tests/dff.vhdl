1dc0abbc6dd20c7b462f8b9bd18557ef6cefeb97
Tomasz Hemperek <hemperek@physik.uni-bonn.de>
2019-08-01 10:48:13 +0200

    diff for vhdl and verilog diffrent module name

--- tests/dff.vhdl
+++ tests/dff.vhdl
@@ -28,14 +28,14 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity dff is
+entity dff_vhdl is
   port (
     c : in  std_logic;
     d : in  std_logic;
     q : out std_logic := '0');
-end entity dff;
+end entity dff_vhdl;
 
-architecture rtl of dff is
+architecture rtl of dff_vhdl is
 begin
   -- It is also possible to add an delay of less than one clock period here.
   q <= d when rising_edge(c);
