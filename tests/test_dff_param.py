0614353356956f66ed16605083118ce9f032b5cf
Tomasz Hemperek <hemperek@physik.uni-bonn.de>
2020-10-16 14:40:52 +0200

    Add GHDL parallel test

--- tests/test_dff_param.py
+++ tests/test_dff_param.py
@@ -4,14 +4,14 @@ import os
 
 tests_dir = os.path.dirname(__file__)
 
-# For paraleparallel runs
+# For parallel runs
 # Pre-compile:
 # pytest -m compile test_dff_param.py
 # Run:
 # pytest --workers 2 test_dff_param.py
 # There are possibly better ways to o this
 
-
+@pytest.mark.skipif(os.getenv("SIM") == "ghdl", reason="Verilog not suported")
 @pytest.mark.compile
 def test_complile():
     run(
@@ -41,4 +41,19 @@ def test_dff_verilog_testcase(testcase):
         toplevel="dff_test",
         module="dff_cocotb",
         testcase=testcase,
+    )
+
+
+# For GHDL create new build/direcotry for every run
+@pytest.mark.skipif(os.getenv("SIM") == "verilator", reason="VHDL not suported")
+@pytest.mark.skipif(os.getenv("SIM") == "icarus", reason="VHDL not suported")
+@pytest.mark.parametrize("seed", range(8))
+def test_dff_vhdl_param(seed):
+    run(
+        vhdl_sources=[os.path.join(tests_dir, "dff.vhdl")],
+        toplevel="dff_test_vhdl",
+        module="dff_cocotb",
+        toplevel_lang="vhdl",
+        seed=seed,
+        sim_build="sim_build/"+str(seed)
     )
