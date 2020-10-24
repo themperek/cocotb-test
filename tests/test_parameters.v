7460bee3c63845203d49c99d034e6f72cc5c1d57
Tomasz Hemperek <hemperek@physik.uni-bonn.de>
2020-10-24 11:38:41 +0200

    Add vhdl paramters test

--- tests/test_parameters.v
+++ tests/test_parameters.v
@@ -4,8 +4,8 @@ module test_parameters #(
     parameter WIDTH_IN = 8,
     parameter WIDTH_OUT = 8
 ) (
-    input  [WIDTH_IN-1:0]   IN,
-    output [WIDTH_OUT-1:0]  OUT
+    input  [WIDTH_IN-1:0]   data_in,
+    output [WIDTH_OUT-1:0]  data_out
 );
 
 endmodule
