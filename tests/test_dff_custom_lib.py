965e607b1f837362d0e89d184b24784b7cf21cb1
benbr8 <1520073+benbr8@users.noreply.github.com>
2022-01-03 11:57:16 +0100

    fix test_dff_custom_lib

--- tests/test_dff_custom_lib.py
+++ tests/test_dff_custom_lib.py
@@ -1,17 +1,16 @@
-from typing import OrderedDict
+from collections import OrderedDict
 from cocotb_test.simulator import run
 import pytest
 import os
 
 tests_dir = os.path.dirname(__file__)
 
-@pytest.mark.skipif(os.getenv("SIM") == "verilator", reason="VHDL not suported")
-@pytest.mark.skipif(os.getenv("SIM") == "icarus", reason="VHDL not suported")
+@pytest.mark.skipif(os.getenv("SIM") != "questa", reason="Named libraries only supported for Questa simulator")
 def test_dff_vhdl():
     run(
         vhdl_sources = OrderedDict([
             ("some_lib", [os.path.join(tests_dir, "dff.vhdl")]),
-            ("some_other_lib", [os.path.join(tests_dir, "dff_wrapper.vhdl")])
+            ("some_other_lib", [os.path.join(tests_dir, "dff_wrapper.vhdl")]),
         ]),
         toplevel="dff_wrapper",
         module="dff_cocotb",
