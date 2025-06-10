83a862ad4cf893787e6d6c31469808e97bb93a19
Kaleb Barrett <dev.ktbarrett@gmail.com>
2025-06-10 10:26:33 -0600

    Add `packaging` as a dependency to setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,12 @@ setup(
     packages=find_packages(),
     include_package_data = True,
     python_requires=">=3.7",
-    install_requires=["cocotb>=1.5", "pytest", "find_libpython"],
+    install_requires=[
+        "cocotb>=1.5",
+        "pytest",
+        "find_libpython",
+        "packaging",
+    ],
     entry_points={
         "console_scripts": [
             "cocotb-test=cocotb_test.cli:config",
